1031c6202bcd182bdf975b1a44ea88d565bf100a
Artem Buyanov <Pard16@mail.ru>
2023-01-17 17:33:07 +0300

    add Добавлена Задача №2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    System.Console.WriteLine($"Максимальное число равно: {a}");
+}
+else
+{
+    System.Console.WriteLine($"Максимаьное число равно: {b}");
+}
